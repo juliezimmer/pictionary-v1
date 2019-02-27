a500490e6112e0c1398339b7ceb20baa0eb4bacd
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2019-02-27 13:18:18 -0600

    images now display in the browser when a search word is entered in the search bar.

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const ImageList = (props ) => {
-   console.log(props.images);
-   
-   return <div>ImageList</div>;
+   // the arrow function inside ,map() is called with each image that is returned by the unsplash API.
+   // This statement returns an array.
+   // description, id, and urls, have been desctuctured out of the image object
+   const images = props.images.map(({ description, id, urls}) => {
+      return <img alt={description} key={id} src={urls.regular} />
+   });
+
+   return <div>{images}</div>;
 };
 
 export default ImageList;
