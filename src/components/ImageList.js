849b2f24f6066033632d9126190b1070d63082e9
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2019-02-28 10:55:25 -0600

    created card component to display each image returned by API; added some styling to ImageList

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,14 +1,15 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = (props ) => {
-   // the arrow function inside ,map() is called with each image that is returned by the unsplash API.
-   // This statement returns an array.
+   // the arrow function inside .map() is called with each image that is returned by the unsplash API.
    // description, id, and urls, have been desctuctured out of the image object
-   const images = props.images.map(({ description, id, urls}) => {
-      return <img alt={description} key={id} src={urls.regular} />
+   const images = props.images.map((image) => {
+      return <ImageCard key={image.id} image={image}/>
    });
 
-   return <div>{images}</div>;
+   return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
