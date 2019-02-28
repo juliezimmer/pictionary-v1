239e5dcde64b90ed69879f48dac48c3e84d7bd2b
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2019-02-28 12:29:25 -0600

    added imageRefs to calculate the height of images and tile them in the browser

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,13 +1,39 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+   constructor (props) {
+      super (props);
+
+      this.state = {
+         span: 0
+      };
+
+      this.imageRef = React.createRef();
+   }
+
+   componentDidMount() {
+      // when the image loads, this.setSpans is called as a callback function.
+      this.imageRef.current.addEventListener('load', this.setSpans);
+   }
+   // called when image loads  
+   setSpans = () => {
+      const height = this.imageRef.current.clientHeight;
+      
+      const spans = Math.ceil(height / 10);
+      // math.ceil caps the value
+
+      // after spans are calculated, they should be set on the state object:
+      this.setState({ spans });
+   }
+
+
    render (){
       // object destructuring
       const { description, urls } = this.props.image;
       
       return (
-         <div>
-            <img src={urls.regular} alt={description} />
+         <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+            <img ref={this.imageRef} src={urls.regular} alt={description} />
          </div>
       );
    }
